9f54103573341ec9df8f9551f8f98db29e1560ab
MacaquinhoProgramador <guilhermeszumski@gmail.com>
2025-05-20 21:35:37 -0300

    fileSystem corrigido para todos o CRUD e readline substituido por prompt-sync

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const readline = require('readline');
+const prompt = require('prompt-sync')();
 const fs = require('fs');
+const { exit } = require('process');
+const tarefasArquivo = './tarefas.json';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -9,11 +12,8 @@ const rl = readline.createInterface({
 let tarefa = [];
 
 function lerTarefas() {
-    fs.readFileSync('tarefas.json', (err, data) => {
-        if(!err) {
-            tarefa = JSON.parse(data);
-        }
-    });    
+    const dados = fs.readFileSync(tarefasArquivo, 'utf-8');
+    tarefa = JSON.parse(dados);
 };
 
 function salvarTarefa(tarefa) {
@@ -22,7 +22,21 @@ function salvarTarefa(tarefa) {
 
 function criar() {
     lerTarefas();
-    rl.question('Digite o nome da tarefa: ', (nome) =>{
+
+    let nome = prompt('Digite o nome da tarefa: ');
+    let descricao = prompt('Digite a descrição da tarefa: ');
+    let prazo = prompt('Digite o prazo de entrega ano, mês e dia respectivamente: ');
+    tarefa.push({
+        id: tarefa.length + 1,
+        nomeTarefa: nome,
+        descricaoTarefa: descricao,
+        prazoTarefa: prazo
+    });
+    //console.log(tarefa);
+    salvarTarefa(tarefa);
+    menu();
+
+    /*rl.question('Digite o nome da tarefa: ', (nome) =>{
         rl.question('Digite a descrição da tarefa: ', (descricao) => {
             rl.question('Digite o prazo de entrega ano, mês e dia respectivamente: ', (prazo) => {
                 tarefa.push({
@@ -36,19 +50,19 @@ function criar() {
                 menu();
             });
         });
-    });
+    });*/
 };
 
 function listar() {
     lerTarefas();
-    console.log(tarefa)
+    //console.log(tarefa)
     tarefa.forEach((index) => {
         console.log(`
-            --------------------------------\n
-            ID: ${index.id}\n
-            tarefa: ${index.nomeTarefa}\n
-            descrição: ${index.descricaoTarefa}\n
-            prazo: ${index.prazo}
+            --------------------------------
+            ID: ${index.id}
+            tarefa: ${index.nomeTarefa}
+            descrição: ${index.descricaoTarefa}
+            prazo: ${index.prazoTarefa}
             `);
     });
     console.log('           --------------------------------')
@@ -56,25 +70,47 @@ function listar() {
 
 function editar() {
     listar();
-    rl.question('Selecione qual tarefa você deseja alterar: ', (entrada) => {
+    let opcao = prompt('Selecione qual tarefa você deseja alterar: ');
+    let idEditar = parseInt(opcao);
+    let novoNome = prompt('Digite o novo nome da tarefa: ');
+    let novaDescricao = prompt('Digite a nova descrição: ');
+    let novoPrazo = prompt('Digite o novo prazo: ');
+    tarefa[idEditar - 1] = {
+        id: opcao,
+        nomeTarefa: novoNome,
+        descricaoTarefa: novaDescricao,
+        prazoTarefa: novoPrazo
+    };
+    //console.log(tarefa);
+    salvarTarefa(tarefa);
+    menu();
+
+    /*rl.question('Selecione qual tarefa você deseja alterar: ', (entrada) => {
         let idEditar = parseInt(entrada);
         rl.question('Digite a alteração: ', (novoNome) => {
             tarefa[idEditar - 1] = {id: entrada, nomeTarefa: novoNome};
             console.log(tarefa);
             menu();
         })
-    });
+    });*/
 }
 
 function deletar() {
     listar();
-    rl.question('Selecione qual tarefa deseja deletar: ', (entrada) => {
+    let opcao = prompt('Selecione qual tarefa deseja deletar: ');
+    let idDeletar = parseInt(opcao);
+    //console.log(idDeletar);
+    tarefa.splice(idDeletar - 1, 1);
+    //console.log(tarefa);
+    salvarTarefa(tarefa);
+    menu();
+    /*.question('Selecione qual tarefa deseja deletar: ', (entrada) => {
         let idDeletar = parseInt(entrada);
         console.log(idDeletar);
         tarefa.splice(idDeletar - 1, 1);
         console.log(tarefa);
         menu();
-    })
+    })*/
 }
 
 function menu() {
@@ -86,7 +122,35 @@ function menu() {
         4. Deletar
         5. Sair
         `);
-        rl.question('Opção: ', (entrada) => {
+        let opcao = prompt('Opção: ');
+        switch (opcao) {
+            case '1':
+                criar();
+                break;
+            
+            case '2':
+                listar();
+                menu();
+                break;
+
+            case '3':
+                editar();
+                break;
+
+            case '4':
+                deletar();
+                break;
+
+            case '5':
+                exit();
+        
+            default:
+                console.log('Valor incorreto!');
+                menu();
+                break;
+        }
+
+        /*rl.question('Opção: ', (entrada) => {
             switch (entrada) {
                 case '1':
                     criar();
@@ -109,7 +173,7 @@ function menu() {
                     rl.close();
                     break;
             }
-        });
+        });*/
 }
 
 menu();
