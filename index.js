6eb085965a85a0dff69fc3644aa55b18aa079053
MacaquinhoProgramador <guilhermeszumski@gmail.com>
2025-05-26 20:45:16 -0300

    Códigos antigos comentados removidos e comentarios explicando o código adicionados

--- index.js
+++ index.js
@@ -9,53 +9,47 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+//Variável tarefa sendo declarada
 let tarefa = [];
 
+//Função de ler arquivo .JSON das tarefas e passar para a variável
 function lerTarefas() {
     const dados = fs.readFileSync(tarefasArquivo, 'utf-8');
     tarefa = JSON.parse(dados);
 };
 
+//Função de salvar os dados da váriavel tarefa dentro do arquivo .JSON das tarefas
 function salvarTarefa(tarefa) {
     fs.writeFileSync('tarefas.json', JSON.stringify(tarefa, null, 2));
 };
 
+// Função de criar nova tarefa
 function criar() {
-    lerTarefas();
+    lerTarefas();   //Chama a função lerTarefas para utilizar o array do arquivo .JSON
 
+    //recebe os dados da nova tarefa
     let nome = prompt('Digite o nome da tarefa: ');
     let descricao = prompt('Digite a descrição da tarefa: ');
     let prazo = prompt('Digite o prazo de entrega ano, mês e dia respectivamente: ');
+    
+    //adiciona os novos dados dentro da array de tarefas
     tarefa.push({
         id: tarefa.length + 1,
         nomeTarefa: nome,
         descricaoTarefa: descricao,
         prazoTarefa: prazo
     });
-    //console.log(tarefa);
-    salvarTarefa(tarefa);
-    menu();
 
-    /*rl.question('Digite o nome da tarefa: ', (nome) =>{
-        rl.question('Digite a descrição da tarefa: ', (descricao) => {
-            rl.question('Digite o prazo de entrega ano, mês e dia respectivamente: ', (prazo) => {
-                tarefa.push({
-                    id: tarefa.length + 1,
-                    nomeTarefa: nome,
-                    descricaoTarefa: descricao,
-                    prazoTarefa: prazo
-                });
-                console.log(tarefa);
-                salvarTarefa(tarefa);
-                menu();
-            });
-        });
-    });*/
+    salvarTarefa(tarefa);   //Chama a função salvarTarefas com a variável tarefas atualizada (sobrescreve a array antiga com a atualizada)
+    menu();     //Retorna ao menu
+
 };
 
+//Função de listar tarefas
 function listar() {
-    lerTarefas();
-    //console.log(tarefa)
+    lerTarefas();       //Chama a função ler tarefas para receber a array das tarefas
+
+    //busca os dados da array e lista cada campo de forma individual e organizado
     tarefa.forEach((index) => {
         console.log(`
             --------------------------------
@@ -68,51 +62,43 @@ function listar() {
     console.log('           --------------------------------')
 }
 
+//Função de editar uma tarefa específica
 function editar() {
-    listar();
-    let opcao = prompt('Selecione qual tarefa você deseja alterar: ');
+
+    listar();   //Chama a função listar para mostrar opções disponiveis de tarefas para realizar a edição
+
+    let opcao = prompt('Selecione qual tarefa você deseja alterar: ');      //Recebe o id de qual tarefa será editada
+
+    //recebe os valores da tarefa editada
     let idEditar = parseInt(opcao);
     let novoNome = prompt('Digite o novo nome da tarefa: ');
     let novaDescricao = prompt('Digite a nova descrição: ');
     let novoPrazo = prompt('Digite o novo prazo: ');
+
+    //Adiciona os valores editados à tarefa específica
     tarefa[idEditar - 1] = {
         id: opcao,
         nomeTarefa: novoNome,
         descricaoTarefa: novaDescricao,
         prazoTarefa: novoPrazo
     };
-    //console.log(tarefa);
-    salvarTarefa(tarefa);
-    menu();
 
-    /*rl.question('Selecione qual tarefa você deseja alterar: ', (entrada) => {
-        let idEditar = parseInt(entrada);
-        rl.question('Digite a alteração: ', (novoNome) => {
-            tarefa[idEditar - 1] = {id: entrada, nomeTarefa: novoNome};
-            console.log(tarefa);
-            menu();
-        })
-    });*/
+    salvarTarefa(tarefa);   //Salva os dados atualizados
+    menu();     //Retorna ao menu
 }
 
+//Função de deletar tarefa
 function deletar() {
-    listar();
-    let opcao = prompt('Selecione qual tarefa deseja deletar: ');
-    let idDeletar = parseInt(opcao);
-    //console.log(idDeletar);
-    tarefa.splice(idDeletar - 1, 1);
-    //console.log(tarefa);
-    salvarTarefa(tarefa);
-    menu();
-    /*.question('Selecione qual tarefa deseja deletar: ', (entrada) => {
-        let idDeletar = parseInt(entrada);
-        console.log(idDeletar);
-        tarefa.splice(idDeletar - 1, 1);
-        console.log(tarefa);
-        menu();
-    })*/
+    listar();       //Chama a função de listar
+
+    let opcao = prompt('Selecione qual tarefa deseja deletar: ');   //Recebe o id da tarefa a ser editada
+    let idDeletar = parseInt(opcao);    //Converte o valor em string para inteiro
+    tarefa.splice(idDeletar - 1, 1);    //Remove o tarefa selecionada
+    salvarTarefa(tarefa);   //salva as tarefas atualizadas sem a tarefa deletada
+    menu();     //retorna ao menu
 }
 
+//Função que abre uma tela de menu
 function menu() {
     console.log(`
         Teste
@@ -123,6 +109,8 @@ function menu() {
         5. Sair
         `);
         let opcao = prompt('Opção: ');
+
+        //Direciona qual função chamar a partir do valor escolhido
         switch (opcao) {
             case '1':
                 criar();
@@ -149,31 +137,6 @@ function menu() {
                 menu();
                 break;
         }
-
-        /*rl.question('Opção: ', (entrada) => {
-            switch (entrada) {
-                case '1':
-                    criar();
-                    break;
-                case '2':
-                    listar();
-                    menu();
-                    break;
-                case '3':
-                    editar();
-                    break;
-                case '4':
-                    deletar();
-                    break;
-                case '5':
-                    rl.close();
-                    break;
-                default:
-                    console.log('Valor incorreto.');
-                    rl.close();
-                    break;
-            }
-        });*/
 }
 
-menu();
+menu();     //chama a função menu
